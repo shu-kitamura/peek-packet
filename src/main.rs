d2c9c7f6d3f3f40990adf8329dfad2d5ba6c06df
shu-kitamura <shusei3316@yahoo.co.jp>
2025-03-20 23:58:24 +0900

    add: main loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 mod device;
 mod handler;
 
+use handler::handle_ethernet_frame;
+use pnet_datalink::{
+    NetworkInterface,
+    Channel::Ethernet,
+};
+
 fn main() {
-    if let Some(nic) = device::get_network_interface("eth0") {
-         println!("{:?}", nic);
+    let nic: NetworkInterface  = match device::get_network_interface("eth0") {
+        Some(iface) => iface,
+        None => return
     };
+
+    let (_, mut rx) = match pnet_datalink::channel(&nic, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type."),
+        Err(e) => panic!("Failed to create channel. {}", e),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(bytes) => handle_ethernet_frame(bytes),
+            Err(e) => panic!("{e}")
+        }
+    }
 }
